109a41b170a8d6f6b78adfeaee52c46e76e94eb9
Crowdbotics <engineering@crowdbotics.com>
2022-09-08 10:29:17 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tetatfunct_dev_32586/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tetatfunct_dev_32586;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tetatfunct_dev_32586";
+  }
+}
